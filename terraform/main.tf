d99d8fdd1af979d58c33685a15d8e9d7defc78f0
Meder <mederemilev04@gmail.com>
2026-05-15 18:10:58 -0500

    Open SSH to 0.0.0.0/0 for GitHub Actions deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_allowed_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
